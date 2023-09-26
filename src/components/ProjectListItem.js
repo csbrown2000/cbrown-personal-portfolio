90635b460394e3b4c590dac8ddd3f9118eb656d2
Collin Brown <csbrown2000@icloud.com>
2023-09-26 08:28:21 -0600

    Adding initial projectpage and project list item files

---
+++ src/components/ProjectListItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProjectListItem(props) {
+	return ( 
+			<div className="list-item">
+				<h1>Project List Item</h1>
+			</div>
+	 );
+}
+
+export default ProjectListItem;
